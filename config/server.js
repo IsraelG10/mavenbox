1481187949634b9ba2267a5bfda1e7c462660ff8
Bryan Gualan <israelg1058@gmail.com>
2021-01-15 13:56:29 -0500

    start

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2813777d9182ba9f277d84c0302aab8d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '96f46650-2140-4e64-b303-750f13a38eab'
+};
